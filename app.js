c1e100ebdb3e4e782160ab4383b4031b7a2822c9
Krish <krish1604mehta@gmail.com>
2025-06-03 17:38:58 +0530

    new  origins

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const allowedOrigins = [
+  'https://main.d3zzjjalhmqmq.amplifyapp.com',
+  "http://localhost:3000"
+];
 
-app.use(cors({origin: 'https://main.d3zzjjalhmqmq.amplifyapp.com'}));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const userRoutes = require('./routes/users');
 
